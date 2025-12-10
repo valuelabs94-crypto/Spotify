053c6e4a14e7f610a9e22faa0e0f1983c279fee9
Prakhar Srivastava <prakharvish19@gmail.com>
2025-12-10 07:46:14 +0000

    Added extra space

--- Manual Scripts/External Stages/albums_data.sql
+++ Manual Scripts/External Stages/albums_data.sql
@@ -1,3 +1,3 @@
-create or replace external table SPOTIFY.EXTERNAL_TABLE.ALBUMS_DATA
+create or replace external table  SPOTIFY.EXTERNAL_TABLE.ALBUMS_DATA
 location=@SPOTIFY.STAGES.SPOTIFY_ALBUM/
 FILE_FORMAT = (FORMAT_NAME = 'my_csv_format');
